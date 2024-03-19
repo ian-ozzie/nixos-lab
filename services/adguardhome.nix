f8d4da0955169b3e089460cd81b8fa005aca5eaf
Ian Oswald <ian@ozzie.io>
2024-03-19 08:36:49 +0000

    adguardhome: init

---
+++ services/adguardhome.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  ...
+}:
+{
+  services = {
+    adguardhome = {
+      allowDHCP = false;
+      enable = true;
+      mutableSettings = false;
+      openFirewall = false;
+      package = with pkgs; adguardhome;
+
+      settings = {
+        schema_version = 24;
+        theme = "dark";
+        users = [ ];
+
+        dns = {
+          bind_hosts = [ "0.0.0.0" ];
+          fallback_dns = [ "https://9.9.9.9/dns-query" ];
+          ports = "53";
+          upstream_dns = [ "https://1.1.1.1/dns-query" ];
+
+          bootstrap_dns = [
+            "1.1.1.1"
+            "9.9.9.9"
+          ];
+        };
+
+        http = {
+          address = "127.0.0.80:8053";
+        };
+      };
+    };
+  };
+}
