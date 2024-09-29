4322b200840d7477d76f53911afafabc74d8912f
Ian Oswald <ian@ozzie.io>
2024-09-29 17:22:19 +1000

    adguardhome: add to flake

--- services/adguardhome.nix
+++ services/adguardhome.nix
@@ -1,40 +1,66 @@
 {
+  config,
+  lib,
   pkgs,
   ...
 }:
+let
+  inherit (config.ozzie.lab) traefik;
+
+  cfg = config.ozzie.lab.adguardhome;
+in
 {
-  networking.firewall = {
-    allowedTCPPorts = [ 53 ];
-    firewall.allowedUDPPorts = [ 53 ];
+  options.ozzie.lab.adguardhome = {
+    enable = lib.mkEnableOption "opinionated adguardhome config";
   };
 
-  services = {
-    adguardhome = {
-      allowDHCP = false;
-      enable = true;
-      mutableSettings = false;
-      openFirewall = false;
-      package = with pkgs; adguardhome;
+  config = lib.mkIf cfg.enable {
+    services = {
+      adguardhome = {
+        allowDHCP = false;
+        enable = true;
+        host = "127.0.0.80";
+        mutableSettings = false;
+        openFirewall = false;
+        package = with pkgs; adguardhome;
+        port = 8053;
 
-      settings = {
-        schema_version = 24;
-        theme = "dark";
-        users = [ ];
+        settings = {
+          schema_version = 28;
+          theme = "dark";
+          users = [ ];
 
-        dns = {
-          bind_hosts = [ "0.0.0.0" ];
-          fallback_dns = [ "https://9.9.9.9/dns-query" ];
-          ports = "53";
-          upstream_dns = [ "https://1.1.1.1/dns-query" ];
+          dns = {
+            bind_hosts = [ "127.0.0.80" ];
+            fallback_dns = [ "https://9.9.9.9/dns-query" ];
+            ports = "53";
+            upstream_dns = [ "https://1.1.1.1/dns-query" ];
 
-          bootstrap_dns = [
-            "1.1.1.1"
-            "9.9.9.9"
-          ];
+            bootstrap_dns = [
+              "1.1.1.1"
+              "9.9.9.9"
+            ];
+          };
+
+          filtering = {
+            filtering_enabled = true;
+            filters_update_interval = 168;
+          };
         };
+      };
 
-        http = {
-          address = "127.0.0.80:8053";
+      traefik = lib.mkIf traefik.enable {
+        dynamicConfigOptions.http = {
+          routers.dns-web = {
+            entryPoints = "websecure";
+            priority = "10";
+            rule = "Host(`dns.${config.ozzie.lab.host.bind.domain}`)";
+            service = "dns-web@file";
+          };
+
+          services.dns-web = {
+            loadBalancer.servers = [ { url = "http://127.0.0.80:8053"; } ];
+          };
         };
       };
     };
--- services/default.nix
+++ services/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./adguardhome.nix
     ./traefik.nix
   ];
 }
