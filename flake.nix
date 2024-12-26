d6bec0432f3d8e4265eafaff747743e39a6dae4c
Ian Oswald <ian@ozzie.io>
2024-12-26 16:11:15 +1100

    flake: update to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Ozzie's NixOS lab configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
   outputs = _: {
