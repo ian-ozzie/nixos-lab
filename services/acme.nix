7e2b7987ff389f79020b3cf846c6a650e1ab3dfb
Ian Oswald <ian@ozzie.io>
2024-12-27 20:37:12 +1100

    acme: add to flake

--- services/acme.nix
+++ services/acme.nix
@@ -1,14 +1,55 @@
 {
-  security.acme = {
-    acceptTerms = true;
-    preliminarySelfsigned = false;
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (config.ozzie.lab) traefik;
 
-    defaults = {
-      dnsPropagationCheck = true;
-      dnsProvider = "cloudflare";
-      dnsResolver = "1.1.1.1:53";
-      environmentFile = "/data/services/acme/.env";
-      group = "acme";
+  cfg = config.ozzie.lab.acme;
+in
+{
+  options.ozzie.lab.acme = {
+    enable = lib.mkEnableOption "opinionated acme config";
+  };
+
+  config = lib.mkIf cfg.enable {
+    security = {
+      acme = {
+        acceptTerms = true;
+        preliminarySelfsigned = false;
+
+        certs."${config.ozzie.lab.host.bind.domain}" = {
+          domain = "${config.ozzie.lab.host.bind.domain}";
+          extraDomainNames = [ "*.${config.ozzie.lab.host.bind.domain}" ];
+        };
+
+        defaults = {
+          dnsPropagationCheck = true;
+          dnsProvider = "cloudflare";
+          dnsResolver = "1.1.1.1:53";
+          environmentFile = "/data/services/acme/.env";
+          group = "acme";
+        };
+      };
+    };
+
+    services = {
+      traefik = lib.mkIf traefik.enable {
+        dynamicConfigOptions.tls.certificates = [
+          {
+            certFile = "/var/lib/acme/${config.ozzie.lab.host.bind.domain}/fullchain.pem";
+            keyFile = "/var/lib/acme/${config.ozzie.lab.host.bind.domain}/key.pem";
+            stores = "default";
+          }
+        ];
+      };
+    };
+
+    users.users = {
+      traefik = lib.mkIf traefik.enable {
+        extraGroups = [ "acme" ];
+      };
     };
   };
 }
