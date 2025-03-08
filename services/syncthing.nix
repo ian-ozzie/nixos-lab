2c5f1df866581b4383f5b764ea80f5d41f0078c4
Ian Oswald <ian@ozzie.io>
2025-03-09 10:19:58 +1100

    syncthing: init

---
+++ services/syncthing.nix
@@ -0,0 +1,65 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (config.ozzie.lab) traefik;
+
+  cfg = config.ozzie.lab.syncthing;
+in
+{
+  options.ozzie.lab.syncthing = {
+    enable = lib.mkEnableOption "opinionated syncthing config";
+
+    group = lib.mkOption {
+      default = config.services.syncthing.group;
+      description = "Group to run syncthing under";
+      type = lib.types.str;
+    };
+
+    user = lib.mkOption {
+      default = config.services.syncthing.user;
+      description = "User to run syncthing under";
+      type = lib.types.str;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services = {
+      syncthing = {
+        inherit (cfg) group user;
+
+        configDir = "/data/services/syncthing/config";
+        dataDir = "/data/services/syncthing";
+        enable = true;
+        guiAddress = "127.0.0.80:8384";
+        overrideDevices = false;
+        overrideFolders = false;
+        package = with pkgs; syncthing;
+        relay.enable = false;
+
+        settings.gui = {
+          insecureAdminAccess = true;
+          insecureSkipHostcheck = true;
+        };
+      };
+
+      traefik = lib.mkIf traefik.enable {
+        dynamicConfigOptions.http = {
+          routers.syncthing = {
+            entryPoints = "websecure";
+            priority = "10";
+            rule = "Host(`sync.${config.ozzie.lab.host.bind.domain}`)";
+            service = "syncthing@file";
+          };
+
+          services.syncthing = {
+            loadBalancer.servers = [ { url = "http://127.0.0.80:8384"; } ];
+          };
+        };
+      };
+    };
+  };
+}
